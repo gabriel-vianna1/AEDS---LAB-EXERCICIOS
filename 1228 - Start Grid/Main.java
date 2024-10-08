b93434ffc2cb0fc6120409b4399e55cfadd2376c
Gabriel Vianna <gabrielvianna495@gmail.com>
2024-10-08 10:25:58 -0300

    lab07

--- 1228 - Start Grid/Main.java
+++ 1228 - Start Grid/Main.java
@@ -10,21 +10,26 @@ class Main{
       int[]  arrayUltrapassagem = new int[3];
     
       while(sc.hasNextLine()){
-    
       numCompetidor = sc.nextInt();
-      //int[] idCompetidores = new int[numCompetidor];
+      sc.nextLine();//Consume a linha em branco depois do nextInt
+     
       //recebe a ordem em uma linha e transoforma p array
-      // pegar as diferenças entre a posição que o numero esta
       String entradaSaida = sc.nextLine();
-      String[] ordemSaida = entradaSaida.split(" ");
+      String[] ordemSaida = entradaSaida.trim().split(" ");
+
       String entradaChegada = sc.nextLine();
-      String[] ordemChegada = entradaChegada.split(" ");
-    
+      String[] ordemChegada = entradaChegada.trim().split(" ");
+      // zera as ultrapassagens depois de cada iteração
+      ultrapassagens = 0;
+      // pegar as diferenças entre a posição que o numero esta
       for(int i = 0; i < numCompetidor; i++){
         String tmp =  ordemSaida[i];
         for(int j = 0; j < numCompetidor; j++){
-            if(ordemChegada[j].equals(tmp) && j - i > 0){
-                ultrapassagens += j - i;
+            if(ordemChegada[j].equals(tmp) && i - j > 0){
+                ultrapassagens += i - j;
+            }
+            else if(ordemChegada[j].equals(tmp) && i - j == 0 && j != (ordemSaida.length - 1) && i != 0 && ordemChegada[j + 1].equals(ordemSaida[i - 1])){
+                ultrapassagens += 1;
             }
         }
     }
@@ -33,8 +38,8 @@ k++;
 
 }
 //Imprime o numero de ultrapassagens      
-for(int i = 0; i < numCompetidor; i++){
-System.out.println(arrayUltrapassagem[i]);
+for(int numero : arrayUltrapassagem){
+System.out.println(numero);
 }
 
 sc.close();
