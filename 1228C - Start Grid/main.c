f87e10ce53eb2a6ed28b20c801f5fbdb46697256
Gabriel Vianna <gabrielvianna495@gmail.com>
2024-10-14 20:48:39 -0300

    Upload LAB07 - em C

---
+++ 1228C - Start Grid/main.c
@@ -0,0 +1,54 @@
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <string.h>
+    
+    int main(){
+
+    int numCompetidor = 0;
+    int ultrapassagens = 0;
+    int k = 0;
+    int arrayUltrapassagens[3];
+    char linha[50];
+    
+    //Le o numero e o proximo caractere, mas descarta o próximo caractere
+    while(scanf("%d", &numCompetidor) != EOF){
+    int ordemSaida[numCompetidor];   
+    int ordemChegada[numCompetidor]; 
+      //Ler a ordem de saida
+    for(int i = 0; i < numCompetidor; i++){
+        scanf("%d", &ordemSaida[i]);
+    } 
+
+    //Ler a ordem de entrada
+    for(int i = 0; i < numCompetidor; i++){
+        scanf("%d", &ordemChegada[i]);
+    } 
+
+
+    
+  // zerar as ultrapassagens para cada entrada
+  ultrapassagens = 0;
+  // pegar as diferenças entre a posição que o numero esta, for duplo, logo : O(N2)
+      for(int i = 0; i < numCompetidor; i++){
+      int tmp = ordemSaida[i];
+        for(int j = 0; j < numCompetidor - 1; j++){
+            if(ordemChegada[j] == tmp && i - j > 0){
+                ultrapassagens += (i - j);
+  }
+          else if(ordemChegada[j] == tmp && i - j == 0  && i != 0 && ordemChegada[j + 1] == ordemSaida[i - 1]){
+              ultrapassagens++;
+      } 
+    }
+  }
+
+  arrayUltrapassagens[k] = ultrapassagens;
+  k++;
+    }
+
+
+
+  for(int i = 0; i < 3; i++){
+      printf("%i\n", arrayUltrapassagens[i]);
+  } 
+    return 0;
+  }
